429ef299062c5e8f4742160e152743b361162010
Balagurusiva <balagurusiva2002@gmail.com>
2026-03-15 12:13:06 +0530

    Validate middleware updated with query and params

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,13 +1,50 @@
-import { NextFunction, Request, Response } from "express";
-import { ZodType } from "zod";
+import { Request, Response, NextFunction } from "express";
+import { ZodType, ZodError } from "zod";
 
-export const validate = (schema: ZodType) => {
+export const validate = (schema: ZodType<any, any, any>) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
-            await schema.parseAsync({ body: req.body })
-            next()
+            const validatedData = await schema.parseAsync({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            }) as { body?: any; query?: any; params?: any };
+
+            if (validatedData.body) {
+                req.body = validatedData.body;
+            }
+
+            if (validatedData.query) {
+                Object.defineProperty(req, 'query', {
+                    value: validatedData.query,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                });
+            }
+
+            if (validatedData.params) {
+                Object.defineProperty(req, 'params', {
+                    value: validatedData.params,
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                });
+            }
+
+            next();
         } catch (error) {
-            next(error)
+            if (error instanceof ZodError) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Validation failed",
+                    errors: error.issues.map((issue) => ({
+                        field: issue.path.join("."),
+                        message: issue.message,
+                    })),
+                });
+            }
+            next(error);
         }
-    }
-}
+    };
+};
